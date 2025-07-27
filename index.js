374048084a03221a10bac7115cefeafe87c7f0e4
orbitmaxlabs <reallyknot1@gmail.com>
2025-07-27 19:02:13 -0400

    fix: register auth alias for root component

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 import { registerRootComponent } from 'expo';
+import { AppRegistry } from 'react-native';
 
 import App from './App';
 
@@ -6,3 +7,8 @@ import App from './App';
 // It also ensures that whether you load the app in Expo Go or in a native build,
 // the environment is set up appropriately
 registerRootComponent(App);
+
+// Some build configurations attempt to start the app using the component name
+// "auth" instead of the default "main". Registering this alias avoids runtime
+// errors like "Component auth has not been registered yet".
+AppRegistry.registerComponent('auth', () => App);
